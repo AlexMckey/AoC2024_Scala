271d48b123ec0ad554d8902ac1e4db8c78c3577b
Alex Mc'key <mckey2002@yandex.ru>
2024-12-01 12:57:55 +0300

    Day01 another small simplification

--- src/year2024/day01.scala
+++ src/year2024/day01.scala
@@ -3,25 +3,23 @@ package year2024.day01
 import puzzle.Puzzle
 import parse.{*, given}
 
-//import scala.math.abs
-
 type I = List[List[Int] - """\s+"""] - "\n"
 
 object Day01 extends Puzzle[I](2024, 1, "Historian Hysteria"):
 
   override def part1(prs: I): Int =
-    val ids = prs.transpose
-    ids.head.sorted
-       .zip(ids.last.sorted)
+    val List(ids1, ids2) = prs.transpose
+    ids1.sorted
+       .zip(ids2.sorted)
        .map((a, b) => (a - b).abs)
        .sum
 
   override def part2(prs: I): Int =
-    val ids    = prs.transpose
-    val idsCnt = ids.last.groupMapReduce(identity)(_ => 1)(_ + _).withDefaultValue(0)
-    ids.head
-       .map(id => id * idsCnt(id))
-       .sum
+    val List(ids1, ids2) = prs.transpose
+    val idsCnt = ids2.groupMapReduce(identity)(_ => 1)(_ + _).withDefaultValue(0)
+    ids1
+      .map(id => id * idsCnt(id))
+      .sum
 
 //Day 1: Historian Hysteria
 //  parse : 35.9ms
