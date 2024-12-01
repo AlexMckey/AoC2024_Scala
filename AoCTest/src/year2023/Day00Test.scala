5dfa7a6f011be6d267336211ee9133ea8e6ae104
Alex Mc'key <mckey2002@yandex.ru>
2024-12-01 12:39:43 +0300

    Initial commit for 2024 year

---
+++ AoCTest/src/year2023/Day00Test.scala
@@ -0,0 +1,9 @@
+package year2023
+
+import test.*
+import parse.given
+import day00.*
+
+object Day00_S1 extends PuzzleStringTest(Day00,"Просто проверка",Some(15),Some(14))
+
+object Day00_S2 extends PuzzleFileTest(Day00,Some(24),Some(19),"Ex1")
