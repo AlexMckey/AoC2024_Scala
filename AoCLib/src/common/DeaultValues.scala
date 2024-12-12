46d518eed7cf9546ec228db33cfc495b0606c31d
Alex Mc'key <mckey2002@yandex.ru>
2024-12-13 00:18:36 +0300

    Change name of the base givens for Default[T]

--- AoCLib/src/common/DeaultValues.scala
+++ AoCLib/src/common/DeaultValues.scala
@@ -3,10 +3,10 @@ package common
 import scala.util.NotGiven
 
 object DefaultValues:
-  given Default[Boolean] = false
-  given Default[Unit] = ()
-  given Default[String] = ""
-  //given Default[Char] = '.'
+  given booleanDefualts: Default[Boolean] = false
+  given unitDefaults: Default[Unit] = ()
+  given stringDefaults: Default[String] = ""
+  given charDefaults: Default[Char] = '.'
   inline given [T](using num: Numeric[T]): Default[T] = num.zero
   given [T]: Default[Option[T]] = None
   given [T]: Default[Seq[T]] = Nil
