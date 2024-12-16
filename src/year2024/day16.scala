64f3cc0df3e04dd85c65d18fc3aafc7a304255a8
Alex Mc'key <mckey2002@yandex.ru>
2024-12-16 23:58:33 +0300

    Some reorganization code for Day16 solution

--- src/year2024/day16.scala
+++ src/year2024/day16.scala
@@ -5,44 +5,48 @@ import parse.{*, given}
 import grid.{CharGrid, VectorGrid}
 import VectorGrid.charVectorGridReader
 import coord.{Dir, Pos}
-import graph.traverse.old.Dijkstra
+import graph.traverse.Dijkstra
 import walker.Walker
 
-type Maze = CharGrid
+case class Maze(g: CharGrid):
+  private lazy val start: Walker = Walker(g.find('S').get, Dir.E)
+  private lazy val endPos: Pos = g.find('E').get
+  private lazy val (path, Some(goal, steps)): (Map[Walker, Int], Option[(Walker, Int)]) =
+    Dijkstra.search(start, _.p == endPos)(ns(_.step)) : @unchecked
+
+  private def ns(go: Walker => Walker)(w: Walker): Set[(Walker, Int)] =
+    val newW = go(w)
+    Set(w.turnLeft -> 1000, w.turnRight -> 1000) ++
+      (if g(newW.p) != '#' then Set(newW -> 1) else Set.empty)
+
+  private def backPaths(w: Walker): Set[Pos] =
+    if w == start
+    then Set(w.p)
+    else
+      val ds = path(w)
+      val ps =
+        for
+          (oldSt, step) <- ns(_.prev)(w)
+          oldD <- path.get(oldSt)
+          if oldD + step == ds
+        yield backPaths(oldSt) + w.p
+      ps.foldLeft(Set.empty)(_ ++ _)
+
+  def stepCnt: Int = steps
+
+  def bestPathCnt: Int = backPaths(goal).size
+
+given Read[Maze] = s => Maze(VectorGrid(s))
 
 object Day16 extends DayOf2024[Maze](16, "Reindeer Maze"):
 
-  def ns(go: Walker => Walker, m: CharGrid)(w: Walker): Seq[(Walker, Int)] =
-    val newW = go(w)
-    Seq(w.turnLeft -> 1000, w.turnRight -> 1000) ++
-      (if m(newW.p) != '#' then Seq(newW -> 1) else Seq.empty)
-
-  def backPaths(m: CharGrid, wi: Map[Walker, Int], finish: Walker, w: Walker): Set[Pos] =
-    if w == finish
-    then Set(w.p)
-    else
-      val ds = wi(w)
-      val ps =
-        for
-          (oldSt, step) <- ns(_.prev, m)(w)
-          oldD <- wi.get(oldSt)
-          if oldD + step == ds
-        yield backPaths(m, wi, finish, oldSt) + w.p
-      ps.foldLeft(Set.empty)(_ ++ _)
-
   override def part1(m: Maze): Long =
-    val start = Walker(m.find('S').get, Dir.E)
-    val end = m.find('E').get
-    Dijkstra.search[Walker](start, _.p == end, ns(_.step, m)).get._2
+    m.stepCnt
 
   override def part2(m: Maze): Long =
-    val start: Walker = Walker(m.find('S').get, Dir.E)
-    val path: Map[Walker, Int] = Dijkstra.traverse[Walker](start, ns(_.step, m))
-    val end: Pos = m.find('E').get
-    val goal = path.filter(_._1.p == end).minBy(_._2)._1
-    backPaths(m, path, start, goal).size
+    m.bestPathCnt
 
 //Day 16: Reindeer Maze
-//  parse : 14.0ms
-//  part 1: 176.ms -> 90440
-//  part 2: 248.ms -> 479
+//  parse : 17.5ms
+//  part 1: 254.ms -> 90440
+//  part 2: 125.ms -> 479
