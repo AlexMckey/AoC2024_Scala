f37461b9b49fb7130d256443be10b5d9b42b8459
Alex Mc'key <mckey2002@yandex.ru>
2024-12-18 20:00:18 +0300

    Remove defect in BinarySearch alg

--- AoCLib/src/math/BinarySearch.scala
+++ AoCLib/src/math/BinarySearch.scala
@@ -5,14 +5,14 @@ import scala.annotation.tailrec
 
 object BinarySearch:
   // Find the value n where p(n) is true and p(n - 1) is false
-  def binarySearch[A, N: Integral](start: N, end: N, result: N => A, p: A => Boolean)
+  def binarySearch[A, N: Integral](start: N, end: N)(result: N => A)(p: A => Boolean)
                                   (using CanEqual[N, N]): (N, A) =
     assert(!p(result(start)), "p(start) must be false")
     assert(p(result(end)), "p(end) must be true")
 
     @tailrec
     def helper(start: N, end: N)(using n: Integral[N]): (N, A) =
-      if end == start + n.one then
+      if end == start then
         (end, result(end))
       else
         val two        = n.fromInt(2)
@@ -21,6 +21,6 @@ object BinarySearch:
         if p(halfResult) then
           helper(start, half)
         else
-          helper(half, end)
+          helper(half + n.one, end)
 
     helper(start, end)
