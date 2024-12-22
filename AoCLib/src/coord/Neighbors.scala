ea04bb642f5a16294ac4685c1bbf53441edc1679
Alex Mc'key <mckey2002@yandex.ru>
2024-12-22 12:24:44 +0300

    Move given neighbors in Object Neighbors

--- AoCLib/src/coord/Neighbors.scala
+++ AoCLib/src/coord/Neighbors.scala
@@ -4,9 +4,10 @@ import coord.{Coord, Pos, given}
 
 trait Neighbors[A]:
   def neighbors(p: Pos, a: A): Iterator[Pos]
-
-given nearNeighbors[A]: Neighbors[A] with
-  override def neighbors(p: Pos, a: A): Iterator[Pos] = p.nearAll
-
-given axisNeighbors[A]: Neighbors[A] with
-  override def neighbors(p: Pos, a: A): Iterator[Pos] = p.nearAxis
+  
+object Neighbors:
+  given nearNeighbors[A]: Neighbors[A] with
+    override def neighbors(p: Pos, a: A): Iterator[Pos] = p.nearAll
+  
+  given axisNeighbors[A]: Neighbors[A] with
+    override def neighbors(p: Pos, a: A): Iterator[Pos] = p.nearAxis
