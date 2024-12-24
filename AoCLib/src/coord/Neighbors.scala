294e724c18d13aa5cfa61f3e18f3df2c37d8476b
Alex Mc'key <mckey2002@yandex.ru>
2024-12-25 00:25:14 +0300

    Change signature given methods of Neighbors

--- AoCLib/src/coord/Neighbors.scala
+++ AoCLib/src/coord/Neighbors.scala
@@ -1,13 +1,15 @@
 package coord
 
 import coord.{Coord, Pos, given}
+import exts.maps.A
 
 trait Neighbors[A]:
   def neighbors(p: Pos, a: A): Iterator[Pos]
   
-object Neighbors:
-  given nearNeighbors[A]: Neighbors[A] with
+object NearNeighbors:
+  given Neighbors[A] with
     override def neighbors(p: Pos, a: A): Iterator[Pos] = p.nearAll
-  
-  given axisNeighbors[A]: Neighbors[A] with
+
+object AxisNeighbors:
+  given Neighbors[A] with
     override def neighbors(p: Pos, a: A): Iterator[Pos] = p.nearAxis
